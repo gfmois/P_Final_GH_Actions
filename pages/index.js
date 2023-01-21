5d39d6d3cb36d76a7691eb01eb4d8bd876a84423
Moises Guerola <daw.moisesguerola@gmail.com>
2023-01-21 15:50:56 +0100

    (GH) - Deploy Vercel - Test 2

--- pages/index.js
+++ pages/index.js
@@ -10,40 +10,12 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">Read this super page!</h1>
+        <h1 className="title">Mira mira como gira!</h1>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Proyecto Realizado por Moisés Guerola - 2Daw
         </p>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <img className="image" style={{ marginTop: "25px" }} src="/golang.png" />
       </main>
 
       <footer>
@@ -192,6 +164,30 @@ export default function Home() {
             flex-direction: column;
           }
         }
+
+        .image {
+          margin-top: 50px;
+          position:fixed;
+          top: 50%;
+          left: 50%;
+          width: 120px;
+          height: 120px;
+          margin: -60px 0 0 -60px;
+          -webkit-animation:spin 2s linear infinite;
+          -moz-animation:spin 2s linear infinite;
+          animation:spin 2s linear infinite;
+      }
+      @-moz-keyframes spin { 
+          100% { -moz-transform: rotate(360deg); } 
+      }
+      @-webkit-keyframes spin { 
+          100% { -webkit-transform: rotate(360deg); } 
+      }
+      @keyframes spin { 
+          100% { 
+              -webkit-transform: rotate(360deg); 
+              transform:rotate(360deg); 
+          
       `}</style>
 
       <style jsx global>{`
