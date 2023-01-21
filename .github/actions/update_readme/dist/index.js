2137378ecec980b960681bf93ce23f3ddf1243bc
Moises Guerola <daw.moisesguerola@gmail.com>
2023-01-21 12:05:28 +0100

    (GH) - Cypress + Badge - Test 14

--- .github/actions/update_readme/dist/index.js
+++ .github/actions/update_readme/dist/index.js
@@ -2821,7 +2821,7 @@ const core = __nccwpck_require__(186)
 const _fs = __nccwpck_require__(147)
 
 const readme = "./README.md"
-const result = core.getInput("result")
+const result = core.getInput("result") || "success"
 let url = "https://img.shields.io/badge/"
 
 const success = "tested%20with-Cypress-04C38E.svg"
@@ -2831,9 +2831,11 @@ url = `${url}${result == "success" ? success : failure}`
 
 _fs.readFile(readme, "utf-8", (err, data) => {
     if (err) throw err;
-
+    
     if (data.search(success) == -1) {
-        data.replace(success, url)
+        data.length == 0
+            ? data += url
+            : data.replace(success, url)
     }
 
     _fs.writeFile(readme, data, (err) => {
