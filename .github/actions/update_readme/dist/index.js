fb62faa1163bc739d16f49fc108708ef5df589e3
Moises Guerola <daw.moisesguerola@gmail.com>
2023-01-21 13:33:17 +0100

    (GH) - Cypress + Badge - Test 17

--- .github/actions/update_readme/dist/index.js
+++ .github/actions/update_readme/dist/index.js
@@ -2829,22 +2829,22 @@ const failure = "https://img.shields.io/badge/test-failure-red"
 let resultBadge = result == 'success' ? success : failure
 
 _fs.readFile(readme, "utf8", (err, data) => {
-    let pos = data.indexOf('[test_result_badge]')
+    let pos = data.indexOf('(test_result_badge)')
 
     console.log(pos);
     
     if (err) throw err;
 
     if (pos != -1) {
-        console.log(`[${resultBadge}]`);
-        data = data.replace('[test_result_badge]', `[${resultBadge}]`)
+        console.log(`(${resultBadge})`);
+        data = data.replace('(test_result_badge)', `(${resultBadge})`)
     } else {
-        if (data.indexOf(`[${failure}]`) != -1 && result == 'success') {
-            data = data.replace(`[${failure}]`, `[${success}]`)
+        if (data.indexOf(`(${failure})`) != -1 && result == 'success') {
+            data = data.replace(`(${failure})`, `(${success})`)
         }
 
-        if (data.indexOf(`[${success}]`) != -1 && result == 'failure') {
-            data = data.replace(`[${success}]`, `[${failure}]`)
+        if (data.indexOf(`(${success})`) != -1 && result == 'failure') {
+            data = data.replace(`(${success})`, `(${failure})`)
         }
     }
 
