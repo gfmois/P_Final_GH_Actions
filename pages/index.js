0ed3fcfdaabf340c22573b53e9aac72969ff42eb
Moises Guerola <daw.moisesguerola@gmail.com>
2023-01-21 15:19:28 +0100

    (GH) - Deploy Vercel - Test 1

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>PROYECTO FINAL GH ACTIONS</title>
+        <title>PROYECTO FINAL GH ACTIONS Moisés Guerola</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
